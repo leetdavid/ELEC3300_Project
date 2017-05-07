92069da128a13d2f9a1b363e744401e0f1655756
David Lee <leetdavid@outlook.com>
2017-05-08 02:52:39 +0800

    Added some simple TEMP init code

--- Project/temp/temp.c
+++ Project/temp/temp.c
@@ -1,6 +1,16 @@
 /* Includes ------------------------------------------------------------------*/
 #include "temp.h"
+#include "ds18b20.h"
 
 void Temp_Init(void) {
-  
+	ds18b20_init(GPIOC, GPIO_Pin_6, TIM2);
+
+
+	//This part is to read the temp value
+	ds18b20_read_temperature_all();
+	ds18b20_wait_for_conversion();
+	printf("%d---\r\n", ds18b20_get_precission());
+	ds18b20_convert_temperature_all();
+
+
 }
