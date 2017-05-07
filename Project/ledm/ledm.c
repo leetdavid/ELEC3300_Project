b4fd0f8cd1022e156edb5214d69670eea896bf50
edugh <edngust@gmail.com>
2017-05-08 06:30:32 +0800

    Implemented DMA for LED Matrix

--- Project/ledm/ledm.c
+++ Project/ledm/ledm.c
@@ -4,17 +4,22 @@
 #include <string.h>
 //#include <stdio.h>
 
-const u8 row_array[] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+const u8 row_array[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
 static u8 row_no;
 static u8 column_block_no;
+static u8 update_count;
 
 void updateDisplay(void){
   for(u8 i = 0; i < 8; i++){
-    led_buffer[i] = 
-        (~(disp_b[i]) & 0x000000FF) << 24 | 
-        (~(disp_r[i]) & 0x000000FF) << 16 | 
-        (~(disp_g[i]) & 0x000000FF) << 8 |
-        1 << i;
+    led_buffer[i*4 + 0] = ~(disp_b[i]);
+    led_buffer[i*4 + 1] = ~(disp_r[i]);
+    led_buffer[i*4 + 2] = ~(disp_g[i]);
+    led_buffer[i*4 + 3] = 1 << i;
+    //led_buffer[i] = 
+    //    (~(disp_b[i]) & 0x000000FF) << 24 | 
+    //    (~(disp_r[i]) & 0x000000FF) << 16 | 
+    //    (~(disp_g[i]) & 0x000000FF) << 8 |
+    //    1 << i;
   }
 }
 
@@ -51,6 +56,7 @@ void setLatchClkPin(u8 value) {
 void LEDM_Init(void) {
   GPIO_InitTypeDef GPIO_InitStructure;
   SPI_InitTypeDef SPI_InitStructure;
+  DMA_InitTypeDef DMA_InitStructure;
   //RCC_PCLK2Config(RCC_HCLK_Div2); 
   
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
@@ -76,13 +82,51 @@ void LEDM_Init(void) {
   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_LSB;
   SPI_Init(SPI1, &SPI_InitStructure); /* Configure SPI1 */
   
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  DMA_DeInit(DMA1_Channel3); 
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)0x4001300C;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)led_buffer;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+  DMA_InitStructure.DMA_BufferSize = 32;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+  DMA_Init(DMA1_Channel3, &DMA_InitStructure);
+  
+  /* Enable SPI_MASTER DMA Tx request */
+  SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Tx, ENABLE);
+  
   SPI_Cmd(SPI1, ENABLE); /* Enable SPI1 */
+  
+  DMA_Cmd(DMA1_Channel3, ENABLE);
+  
+  //DMA_ITConfig(DMA1_Channel3, DMA_IT_TC, ENABLE);
+ 
+  //NVIC_InitTypeDef NVIC_InitStructure;
+  //Enable DMA1 channel IRQ Channel */
+  //NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel3_IRQn;
+  //NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  //NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  //NVIC_Init(&NVIC_InitStructure);
+  
+  //while(!DMA_GetFlagStatus(DMA1_FLAG_TC3));
   
   memset(led_buffer, 0, sizeof(led_buffer));
   row_no = 0;
   column_block_no = 0;
+  update_count = 0;
   
   // test codes  
+  for(int i = 0; i < 8; i++) {
+    disp_r[i] = icons[0][0][i];
+    disp_g[i] = icons[0][1][i];
+    disp_b[i] = icons[0][2][i];
+  }
   //memcpy(disp_r, icons[0][0], sizeof(disp_r) * 8);
   //memcpy(disp_g, icons[0][1], sizeof(disp_g) * 8);
   //memcpy(disp_b, icons[0][2], sizeof(disp_b) * 8);
@@ -94,22 +138,40 @@ void LEDM_Init(void) {
   //LEDM_RTC_Configuration();
 }
 
+//void DMA1_Channel3_IRQHandler(void)
+//{
+  //Test on DMA1 Channel1 Transfer Complete interrupt
+//  if(DMA_GetITStatus(DMA1_IT_TC3))
+//  {
+//    update_count++;
+//    if(update_count >= 32) {
+//      update_count = 0;
+//    }
+    //Clear DMA1 Channel1 Half Transfer, Transfer Complete and Global interrupt pending bits
+//    DMA_ClearITPendingBit(DMA1_IT_GL3);
+//  }
+//}
+
 void update_Buffer(void) {
   u8 send_data;
   for(int i = 0; i < ROW_COUNT; i++) {
     setLatchClkPin(0);
-    send_data = (led_buffer[i] & 0xFF000000) >> 24;
-    SPI_I2S_SendData(SPI1, send_data);
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-    send_data = (led_buffer[i] & 0x00FF0000) >> 16;
-    SPI_I2S_SendData(SPI1, send_data);
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-    send_data = (led_buffer[i] & 0x0000FF00) >> 8;
-    SPI_I2S_SendData(SPI1, send_data);
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-    send_data = (led_buffer[i] & 0x000000FF);
-    SPI_I2S_SendData(SPI1, send_data);
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    for(int j = 0; j < 4; j++) {
+      SPI_I2S_SendData(SPI1, led_buffer[i*4 + j]);
+      while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    }
+    //send_data = (led_buffer[i] & 0xFF000000) >> 24;
+    //SPI_I2S_SendData(SPI1, send_data);
+    //while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    //send_data = (led_buffer[i] & 0x00FF0000) >> 16;
+    //SPI_I2S_SendData(SPI1, send_data);
+    //while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    //send_data = (led_buffer[i] & 0x0000FF00) >> 8;
+    //SPI_I2S_SendData(SPI1, send_data);
+    //while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    //send_data = (led_buffer[i] & 0x000000FF);
+    //SPI_I2S_SendData(SPI1, row_array[i]);
+    //while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
     for (int i=0; i<500; i++);
     setLatchClkPin(1);
   }
